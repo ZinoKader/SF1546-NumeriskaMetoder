7fda687bc0495ea3acb521107f5229718dda95c4
Zino Kader <zino@zinokader.se>
2019-02-02 00:05:22 +0100

    2c) solved

--- lab1/main2.m
+++ lab1/main2.m
@@ -36,11 +36,12 @@ load("eiffel1.mat")
 
 n = 261;
 b = zeros(2*n, 1);
-x_dislocations = [];
+x_dislocations = zeros(n, 1);
 for i = 1:size(b)
     % i b är element med udda index F i x-led, och jämna F i y-led
     % vi vill belasta i y-led, så vi ändrar element m. jämnt index
     if mod(i, 2) == 0
+        node_index = i / 2;
         b(i) = -1;
         x = A\b;
         x_sum = 0;
@@ -50,13 +51,24 @@ for i = 1:size(b)
             x_sum = ((x_delta^2) + (y_delta^2));
         end
         x_abs = sqrt(x_sum);
-        x_dislocations(size(x_dislocations)+1) = x_abs;      
+        x_dislocations(node_index) = x_abs;
     end    
     b = zeros(2*n, 1); % återställ b för nästa provförskjutning
 end    
-x_dislocations;
+[max_dislocation, max_index] = max(x_dislocations);
+[min_dislocation, min_index] = min(x_dislocations);
 
 
+max_y_node = ynod(max_index);
+max_x_node = xnod(max_index);
+
+min_y_node = ynod(min_index);
+min_x_node = xnod(min_index);
+
+trussplot(xnod, ynod, bars)
+hold on
+plot(max_x_node, max_y_node, "ro")
+plot(min_x_node, min_y_node, "r*")
 
 
 
