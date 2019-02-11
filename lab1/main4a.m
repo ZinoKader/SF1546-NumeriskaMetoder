6a66616130045a0756783ab8496427d20e8c1922
Zino Kader <zino@zinokader.se>
2019-02-11 19:29:34 +0100

    konvergensplot.fig added

--- lab1/main4a.m
+++ lab1/main4a.m
@@ -3,18 +3,6 @@ clc
 close all
 figure()
 
-%{
-L = 2.8;
-n_1 = 40;
-n_2 = 80;
-h = L / n_1;
-x_range = 0:h:L;
-
-y = @(x) 8 ./ ((2+x.^2).*(6-3.*cos(pi.*x)));
-plot(x_range, y(x_range))
-axis("equal")
-%}
-
 L = 2.8;
 n_ref = 1000000;
 V_ref = simpson(n_ref);
@@ -37,5 +25,5 @@ end
 
 hold on
 grid on
-plot(log(h_values),log(E_h_values_trapets), "r-", log(h_values), log(h_values.^2), "--", log(h_values), log(E_h_values_simpson), "b-", log(h_values), log(h_values.^4), ":")
-legend('p2', 'Error trapets', 'p4', 'Error simpson')
+plot(log(h_values),log(E_h_values_trapets), log(h_values), log(h_values.^2), "--", log(h_values), log(E_h_values_simpson), log(h_values), log(h_values.^4), ":")
+legend('Error trapets', 'p2','Error simpson', 'p4')
