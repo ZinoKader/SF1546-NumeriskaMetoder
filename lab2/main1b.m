a06168ac0e17f1b22d6f1e78a1dac337b8b849b3
Zino Kader <zino@zinokader.se>
2019-02-19 21:54:27 +0100

    1b) klar men konstig konvergensordning

--- lab2/main1b.m
+++ lab2/main1b.m
@@ -2,44 +2,50 @@
 clc
 %figure()
 
-% test av funktion
+% test av funktioner
 [t, y] = feuler([0.5, 0], 5, 5/0.01);
 plot(t,y(:,1))
-
+hold on
 [t, y] = rk4([0.5, 0], 5, 5/0.01);
 plot(t,y(:,1))
 
 
+kvoter_feuler = [ ];
+kvoter_rk4 = [ ];
 
-kvoter = [ ];
 
+[t_feuler, y_feuler] = feuler([0.5, 0], 5, 5/0.001); 
+[t2_feuler, y2_feuler] = feuler([0.5, 0], 5, 5/0.002);
+[t4_feuler, y4_feuler] = feuler([0.5, 0], 5, 5/0.004);
 
-[t, y] = feuler([0.5, 0], 5, 5/0.01); 
-[t2, y2] = feuler([0.5, 0], 5, 5/0.02);
-[t4, y4] = feuler([0.5, 0], 5, 5/0.04);
-
+[t_rk4, y_rk4] = rk4([0.5, 0], 5, 5/0.001); 
+[t2_rk4, y2_rk4] = rk4([0.5, 0], 5, 5/0.002);
+[t4_rk4, y4_rk4] = rk4([0.5, 0], 5, 5/0.004);
 
 % välj var fjärde för y
 
-y = y(1:4:end,:);
+y_feuler = y_feuler(1:4:end,:);
+y_rk4 = y_rk4(1:4:end,:);
 
 % välj varannan för y2
 
-y2 = y2(1:2:end,:);
+y2_feuler = y2_feuler(1:2:end,:);
+y2_rk4 = y2_rk4(1:2:end,:);
 
 % välj alla för y4 (symboliskt)
 
-y4 = y4;
+y4_feuler = y4_feuler;
+y4_rk4 = y4_rk4;
 
-for i = t4
-    %kvot = abs((y - y2) / (y2 - y4));
-    %kvoter = [kvoter; kvot];
-    
-    kvot = abs( (y - y2) ./ (y2 - y4) );
-    
+for i = t4_feuler
+    kvot_feuler = abs((y_feuler - y2_feuler) ./ (y2_feuler - y4_feuler));
+    kvoter_feuler = [kvoter_feuler; kvot_feuler];
+    kvot_rk4 = abs((y_rk4 - y2_rk4) ./ (y2_rk4 - y4_rk4));
+    kvoter_rk4 = [kvoter_rk4; kvot_rk4];
 end
 
-kvoter_first = abs(log2(kvot(1:10,1)));
+log_kvot_feuler = abs(log2(kvoter_feuler(1:10,1)));
+log_kvot_rk4 = abs(log2(kvoter_rk4(117:126,1)));
 
-kvottabell = [t4(1:10, 1), kvoter_first];
-array2table(kvottabell, 'VariableNames', {'h', 'kvot'})
+kvottabell = [t4_feuler(1:10, 1), log_kvot_feuler, log_kvot_rk4];
+array2table(kvottabell, 'VariableNames', {'h', 'log_kvot_feuler', 'log_kvot_rk4'})
