19838cc303709ba1fa24ce344cc9058bdf2cda2d
Zino Kader <zino@zinokader.se>
2019-02-26 15:47:57 +0100

    3a) och 3b) klara

---
+++ lab2/stav.m
@@ -0,0 +1,35 @@
+
+function [x_vectors, T_vectors] = stav(n, q0, q1)
+    L = 3;
+    k = 2;
+    t0 = 290;
+    t1 = 400;
+    h = L / (n + 1);
+    Q = @(x) q0*exp((-q1*(x-0.7*L)^2)) + 200;
+
+    b = [ ];
+    i = 1;
+    for x = 2:1:n-1
+        b(i) = ((h^2)/k) * Q(x*h);
+        i = i + 1;
+    end
+
+    first = ((h^2)/k) * Q(0) + t0;
+    last = ((h^2)/k) * Q((n+1)*h) + t1;
+
+    b = [first b last];
+
+    twos = ones(n, 1) * 2;
+    minus_ones = ones(n-1, 1) * -1;
+    A = diag(twos, 0) + diag(minus_ones, 1) + diag(minus_ones,-1);
+    A = sparse(A);
+
+    T = A \ b';
+    T = [t0; T; t1]';
+    x_vectors = 0:h:L;
+    T_vectors = T;
+
+
+
+
+
