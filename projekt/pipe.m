47ae8507612e0d954092bda2dcfc08b0fd33dfde
Zino Kader <zino@zinokader.se>
2019-04-16 17:43:29 +0200

    projektstart

---
+++ projekt/pipe.m
@@ -0,0 +1,35 @@
+
+function [T_values] = pipe(n)
+
+    inner_r = 1;
+    outer_r = 2;
+    k = 50;
+    a = 15;
+    Te = 20;
+    Ti = 350;
+    h = (outer_r - inner_r) / (n+1);
+    standard_row = @(r) [r-(h/2), -2*r, r+(h/2)];
+    
+    first_row = zeros(1, n);
+    last_row = zeros(1, n);
+    first_row(1, [1,2]) = [-2*inner_r, inner_r + (h/2)];
+    last_row(1, [n-1, n]) = [ ((4*k)/(2*a*h + 3*k)) - 2 , -k/(2*a*h + 3*k) + 1 ];
+    
+    middle_rows = zeros(n-2,n);
+    offset = 0;
+    row_count = 1;
+    for r = inner_r + h : h : outer_r - h
+        if offset == n-2
+            break
+        end    
+        middle_rows(row_count, [1+offset, 2+offset, 3+offset]) = standard_row(r);
+        row_count = row_count + 1;
+        offset = offset + 1;
+    end
+    A = [first_row; middle_rows; last_row];
+    b = zeros(n-2, 1);
+    b = [-Ti*(inner_r - (h/2)); b; (-2*a*h*Te)/(2*a*h + 3*k)];
+    
+    x = A \ b
+end
+
